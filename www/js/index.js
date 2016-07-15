f7b2dee9cc1a246e41048c89e35eed458eb21a06
Don Coleman <dcoleman@chariotsolutions.com>
2016-07-15 14:21:53 -0400

    ble

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,6 @@
+var SERVICE = 'aa7b3c40-f6ed-4ffc-bc29-5750c59e74b3';
+var CHARACTERISTIC = 'b0351694-25e6-4eb5-918c-ca9403ddac47';
+
 var app = {
     initialize: function() {
         this.bindEvents();
@@ -6,7 +9,43 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
+        app.scan();
+    },
+    scan: function() {
+        var div = document.querySelector('div');
+        div.innerText = "Scanning for " + SERVICE;
+        
+        var scanFailed = function() {
+            div.innerHTML = "Scan failed";
+        };
+        
+        var connectFailed = function() {
+            div.innerHTML = "Failed to connect to device";            
+        };
+        
+        // warning this scans forever if no devices are found
+        var discoveredPeripheral = function(peripheral) {
+            div.innerText = "Found " + peripheral.id;
+            ble.stopScan();
+            ble.connect(peripheral.id, app.onConnected, connectFailed);
+        };
+        
+        ble.startScan([SERVICE], discoveredPeripheral, scanFailed);
+    },
+    onConnected: function(peripheral) {
+        document.querySelector('div').innerText = "Connected " + peripheral.id;
+        app.pre = document.querySelector('pre');
+        var subscribeFailed = function() { pre.innerText = "Error subscribing for notifications"; };
+        ble.startNotification(peripheral.id, SERVICE, CHARACTERISTIC, app.onData, subscribeFailed);
+
+    },
+    onData: function(buffer) {
+        var value = new Uint8Array(buffer)[0];
+        console.log(value);
+        app.pre.innerText = value;
     }
+    
 };
 
 app.initialize();
+
